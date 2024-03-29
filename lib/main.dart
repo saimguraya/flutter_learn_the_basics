3dc7f8f23e02b72c25cb8f5fa6a84ff2eb7b4081
Saim <saimguraya1@gmail.com>
2024-03-29 16:25:41 +0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -56,8 +56,10 @@ class _MyAppState extends State<MyApp> {
                     ],
                   ),
                 )
-              : Image.network(
-                  'https://images.pexels.com/photos/5054358/pexels-photo-5054358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+              : Image.asset(
+                  'images/1.jpg',
+                  fit: BoxFit.fill,
+                ),
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
